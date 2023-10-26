48c43915c4ab7afe6c75c7ec5dc513e377f6c5e4
Peter Kofler <peter.kofler@code-cop.org>
2023-10-26 17:03:05 +0200

    Rename Java sample test.

--- java/src/test/java/sample/SampleTest.java
+++ java/src/test/java/sample/SampleTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class Junit5Test {
+class SampleTest {
 
     @Test
     void shouldAssertPlainJUnit() {
